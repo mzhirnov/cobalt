c123d0732344e1fcd3c2748db569bf39b0652599
Maksim Zhirnov <mzhirnov@gmail.com>
2016-10-31 21:05:45 +0300

    Compile-time assertions in hash unit test

--- unittests/hash.cpp
+++ unittests/hash.cpp
@@ -2,6 +2,9 @@
 #include <cobalt/hash.hpp>
 
 TEST_CASE("hash") {
+	static_assert(cobalt::murmur3_32("Hello, world!", 0) == 3224780355, "");
+	static_assert("Hello, world!"_hash == 3224780355, "");
+	
 	REQUIRE(cobalt::murmur3_32("Hello, world!", 0) == 3224780355);
 	REQUIRE("Hello, world!"_hash == 3224780355);
 }
