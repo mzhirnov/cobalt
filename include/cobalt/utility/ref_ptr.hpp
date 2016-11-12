3a58aa20727b93e99297c4e74c9b710792b9c06d
Maksim Zhirnov <mzhirnov@gmail.com>
2016-11-12 11:20:07 +0300

    Added add_ref/release functions to utility/add_ref

--- include/cobalt/utility/ref_ptr.hpp
+++ include/cobalt/utility/ref_ptr.hpp
@@ -6,14 +6,26 @@
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 
+#include <type_traits>
+
 namespace cobalt {
 
 template <typename T>
 using ref_counter = boost::intrusive_ref_counter<T>;
-	
+
 template <typename T>
 using ref_ptr = boost::intrusive_ptr<T>;
-	
+
+template <typename T, typename = typename std::enable_if_t<std::is_base_of<ref_counter<T>, T>::value>>
+inline void add_ref(T* p) {
+	intrusive_ptr_add_ref(p);
+}
+
+template <typename T, typename = typename std::enable_if_t<std::is_base_of<ref_counter<T>, T>::value>>
+inline void release(T* p) {
+	intrusive_ptr_release(p);
+}
+
 } // namespace cobalt
 
 #endif // COBALT_UTILITY_REF_PTR_HPP_INCLUDED
