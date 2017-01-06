7c9f0653535a22813adb0171188f4d807493d0cd
Maksim Zhirnov <mzhirnov@gmail.com>
2017-01-06 14:49:55 +0300

    Added assertion to throw_error()

--- include/cobalt/utility/throw_error.hpp
+++ include/cobalt/utility/throw_error.hpp
@@ -3,11 +3,13 @@
 #include <boost/exception/all.hpp>
 
 inline void throw_error(const std::error_code& ec) {
+	BOOST_ASSERT(!ec);
 	if (ec)
 		BOOST_THROW_EXCEPTION(std::system_error(ec));
 }
 
 inline void throw_error(const std::error_code& ec, const char* message) {
+	BOOST_ASSERT(!ec);
 	if (ec)
 		BOOST_THROW_EXCEPTION(std::system_error(ec, message));
 }
