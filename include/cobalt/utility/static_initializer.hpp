3f75100256be0142729185d6e07847a4dad5110e
Maksim Zhirnov <mzhirnov@gmail.com>
2017-02-09 20:11:13 +0300

    Working on static_initializer

--- include/cobalt/utility/static_initializer.hpp
+++ include/cobalt/utility/static_initializer.hpp
@@ -6,31 +6,49 @@
 namespace cobalt {
 
 template <typename T>
-struct static_initializer {
-	struct helper {
-		helper& _instantiate_me;
-		
-		helper() : _instantiate_me(_helper)
+class static_initializer {
+public:
+	class helper {
+	public:
+		helper() noexcept
+			: _instance(_helper)
 		{
 			T::static_initialize();
 		}
 		
-		~helper()
+		~helper() noexcept
 		{
 			T::static_uninitialize();
 		}
+		
+	private:
+		helper& _instance;
 	};
-
+	
+	static static_initializer::helper& instance() noexcept {
+		return _helper;
+	}
+	
 private:
 	static helper _helper;
 };
 
 template <typename T> typename static_initializer<T>::helper static_initializer<T>::_helper;
 
-#define DECLARE_STATIC_INITIALIZER(Class) \
+template <typename T>
+class static_initializable {
+public:
+	~static_initializable() noexcept {
+		static_initializer<T>::instance();
+	}
+};
+
+#define CO_STATIC_INITIALIZE(Class) \
 	private: \
-		friend struct static_initializer<Class>; \
-		static static_initializer<Class>::helper initializer() { return {}; } \
+		friend class static_initializer<Class>; \
+		static const typename static_initializer<Class>::helper& initializer() noexcept { \
+			return static_initializer<Class>::instance(); \
+		} \
 	public:
 
 } // namespace cobalt
--- unittests/static_initializer.cpp
+++ unittests/static_initializer.cpp
@@ -4,19 +4,29 @@
 using namespace cobalt;
 
 class my_class {
-	DECLARE_STATIC_INITIALIZER(my_class)
+	CO_STATIC_INITIALIZE(my_class)
 	
 public:
 	static int initialized;
 	
 private:
-	static void static_initialize() { ++initialized; }
-	static void static_uninitialize() { --initialized; }
+	static void static_initialize() { ++initialized; puts(__PRETTY_FUNCTION__); }
+	static void static_uninitialize() { --initialized; puts(__PRETTY_FUNCTION__); }
 };
 
 int my_class::initialized = 0;
 
-TEST_CASE("static_initializer") {
+class my_class2 : public static_initializable<my_class2> {
+public:
+	static int initialized;
+	
+	static void static_initialize() { ++initialized; puts(__PRETTY_FUNCTION__); }
+	static void static_uninitialize() { --initialized; puts(__PRETTY_FUNCTION__); }
+};
+
+int my_class2::initialized = 0;
+
+TEST_CASE("static_initializer with macros") {
 	SECTION("w/o instances") {
 		REQUIRE(my_class::initialized == 1);
 	}
@@ -25,5 +35,17 @@ TEST_CASE("static_initializer") {
 		my_class inst;
 		
 		REQUIRE(my_class::initialized == 1);
+	}
+}
+
+TEST_CASE("static_initializer with inheritance") {
+	SECTION("w/o instances") {
+		REQUIRE(my_class2::initialized == 1);
+	}
+	
+	SECTION("with an instance") {
+		my_class2 inst;
+		
+		REQUIRE(my_class2::initialized == 1);
 	}
 }
