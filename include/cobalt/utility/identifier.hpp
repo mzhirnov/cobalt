fef486ce833aa0a5adccd1c1958abdae2d76c201
Maksim Zhirnov <mzhirnov@gmail.com>
2017-04-08 12:32:37 +0300

    Use murmur3 as hash function for identifier

--- include/cobalt/utility/identifier.hpp
+++ include/cobalt/utility/identifier.hpp
@@ -3,13 +3,31 @@
 
 #pragma once
 
+#include <cobalt/utility/hash.hpp>
+
 #include <boost/flyweight.hpp>
 
 #include <string>
 
 namespace cobalt {
 
-using identifier = boost::flyweight<std::string>;
+struct identifier_tag {};
+
+struct identifier_hash {
+	std::size_t operator()(const std::string& str) const noexcept {
+		return static_cast<std::size_t>(murmur3(str.c_str(), str.size(), 0));
+	}
+};
+
+using identifier = boost::flyweight<
+	std::string,
+	boost::flyweights::tag<identifier_tag>,
+	boost::flyweights::hashed_factory<
+		identifier_hash,
+		std::equal_to<std::string>,
+		std::allocator<boost::mpl::_1>
+	>
+>;
 
 } // namespace cobalt
 
