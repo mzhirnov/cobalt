041439c50fccb43026d220309c9e87fa1dbc03e0
Maksim Zhirnov <mzhirnov@gmail.com>
2017-04-08 11:35:23 +0300

    Fixed includes in utility.hpp

--- include/cobalt/utility.hpp
+++ include/cobalt/utility.hpp
@@ -4,10 +4,11 @@
 #pragma once
 
 #include <cobalt/utility/ref_counter.hpp>
-#include <cobalt/utility/make_intrusive.hpp>
+#include <cobalt/utility/intrusive.hpp>
 #include <cobalt/utility/compare_floats.hpp>
 #include <cobalt/utility/hash.hpp>
 #include <cobalt/utility/enum_traits.hpp>
 #include <cobalt/utility/enumerator.hpp>
+#include <cobalt/utility/factory.hpp>
 
 #endif // COBALT_UTILITY_HPP_INCLUDED
